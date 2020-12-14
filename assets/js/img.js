fd9cc662533fb9c7bc3560d9237a8238d77aab69
Steve <beyer_steve@web.de>
2020-12-14 20:12:41 +0100

    mostly list styles

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const headerImg = document.querySelector('#header-img');
-    const headerContainer = headerImg.closest('header');
-    if (headerContainer) {
-        headerImg.style.display = 'none';
-        headerContainer.style.backgroundImage = 'url("' + (headerImg.currentSrc ? headerImg.currentSrc : headerImg.src) + '")';
-    }
+    const headerImgs = document.querySelectorAll('header img:first-of-type');
+    headerImgs.forEach(headerImg => {
+        const headerContainer = headerImg.closest('header');
+
+        if (headerContainer) {
+            headerImg.style.display = 'none';
+            headerContainer.style.backgroundImage = 'url("' + (headerImg.currentSrc ? headerImg.currentSrc : headerImg.src) + '")';
+        }
+    });
 });
