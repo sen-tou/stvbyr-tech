cc9a38c321816ad6f199f03eb7554833f313fc9f
Steve <beyer_steve@web.de>
2021-02-28 19:07:55 +0100

    change header src on img currentSrc change

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const headerImgs = document.querySelectorAll('header img:first-of-type');
+
+    // ensures this works for some older browsers
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
     headerImgs.forEach(headerImg => {
         const headerContainer = headerImg.closest('header');
 
-        if (headerContainer) {
-            headerImg.style.display = 'none';
-            headerContainer.style.backgroundImage = 'url("' + (headerImg.currentSrc ? headerImg.currentSrc : headerImg.src) + '")';
-        }
+        const setCurrentSrc = function () {
+            if (headerContainer) {
+                headerImg.style.display = 'none';
+                headerContainer.style.backgroundImage = 'url("' + (headerImg.currentSrc ? headerImg.currentSrc : headerImg.src) + '")';
+            }
+        };
+
+        setCurrentSrc();
+        headerImg.addEventListener('load', setCurrentSrc);
     });
 });
