e9d95f48a77550ffbf1c4bb33d8e71200e0118f5
Steve <beyer_steve@web.de>
2021-03-13 13:23:52 +0100

    style: add config and style css and js

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const headerImgs = document.querySelectorAll('header img:first-of-type');
 
-    headerImgs.forEach(headerImg => {
+    headerImgs.forEach((headerImg) => {
         const headerContainer = headerImg.closest('header');
 
         const setCurrentSrc = function () {
             if (headerContainer) {
                 headerImg.style.display = 'none';
-                headerContainer.style.backgroundImage = 'url("' + (headerImg.currentSrc ? headerImg.currentSrc : headerImg.src) + '")';
+                headerContainer.style.backgroundImage =
+                    'url("' +
+                    (headerImg.currentSrc
+                        ? headerImg.currentSrc
+                        : headerImg.src) +
+                    '")';
             }
         };
 
