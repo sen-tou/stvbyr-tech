8e87e7172afb1452a0a95b88f4c5ea77095ba2d1
Steve Beyer <beyer_steve@web.de>
2021-03-27 21:16:40 +0100

    revise: simplified the img partial

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const headerImgs = document.querySelectorAll('header img:first-of-type');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const headerImgs = document.querySelectorAll('header img:first-of-type');
 
-    headerImgs.forEach((headerImg) => {
-        const headerContainer = headerImg.closest('header');
+//     headerImgs.forEach((headerImg) => {
+//         const headerContainer = headerImg.closest('header');
 
-        const setCurrentSrc = function () {
-            if (headerContainer) {
-                headerImg.style.display = 'none';
-                headerContainer.style.backgroundImage =
-                    'url("' +
-                    (headerImg.currentSrc
-                        ? headerImg.currentSrc
-                        : headerImg.src) +
-                    '")';
-            }
-        };
+//         const setCurrentSrc = function () {
+//             if (headerContainer) {
+//                 headerImg.style.display = 'none';
+//                 headerContainer.style.backgroundImage =
+//                     'url("' +
+//                     (headerImg.currentSrc
+//                         ? headerImg.currentSrc
+//                         : headerImg.src) +
+//                     '")';
+//             }
+//         };
 
-        setCurrentSrc();
-        headerImg.addEventListener('load', setCurrentSrc);
-    });
-});
+//         setCurrentSrc();
+//         headerImg.addEventListener('load', setCurrentSrc);
+//     });
+// });
